3fb6f0c2edeafd1fb462ae539125448e49e309fa
berberin <souplua@gmail.com>
2022-06-24 00:28:26 +0700

    fix[agent]: catch exception

--- app/agent.py
+++ app/agent.py
@@ -11,16 +11,16 @@ class Agent:
     def get_url_set(self, request_info_list=[], *args):
         responses_list = []
         for req_info in request_info_list:
-            # try:
-            if (req_info.method == "GET"):
-                resp = self.scraper.get(url=req_info.url)
-                responses_list.append(ResponseInfo(req_info.id, resp.text))
-            if (req_info.method == "POST"):
-                resp = self.scraper.post(
-                    url=req_info.url, data=req_info.data)
-                responses_list.append(ResponseInfo(req_info.id, resp.text))
-        # except:
-        #     responses_list.append(ResponseInfo(
-        #         req_info.id, "Something went wrong."))
+            try:
+                if (req_info.method == "GET"):
+                    resp = self.scraper.get(url=req_info.url)
+                    responses_list.append(ResponseInfo(req_info.id, resp.text))
+                if (req_info.method == "POST"):
+                    resp = self.scraper.post(
+                        url=req_info.url, data=req_info.data)
+                    responses_list.append(ResponseInfo(req_info.id, resp.text))
+            except:
+                responses_list.append(ResponseInfo(
+                    req_info.id, "Something went wrong."))
 
         return responses_list
